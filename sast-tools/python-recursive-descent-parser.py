926f23063deaf7629ef6da6814ab63349ca157e1
Andy D <8628078+rpigu-i@users.noreply.github.com>
2024-08-17 16:10:34 +0000

    wrapped code in a class

--- sast-tools/python-recursive-descent-parser.py
+++ sast-tools/python-recursive-descent-parser.py
@@ -11,82 +11,88 @@
 
 import re
 
-def tokenize(expression):
-    """ 
-    Function to tokenize 
-    the input from the user.
+class RecursiveDescentParser():
     """
-    token_pattern = r"(\d+|[()+\-*/])"
-    tokens = re.findall(token_pattern, expression)
-    return tokens 
-
-def parse_factor(tokens):
+    Recursive Descent Parser class
     """
-    Parse the factors as defined in the example
-    comments at the start of the file:
+    
+    def tokenize(self, expression):
+        """ 
+        Function to tokenize 
+        the input from the user.
+        """
+        token_pattern = r"(\d+|[()+\-*/])"
+        tokens = re.findall(token_pattern, expression)
+        return tokens 
 
-    factor -> ( expression ) | number
-    """    
-    token = tokens.pop(0)
-    if token == "(":
-        result = parse_expression(tokens)
-        tokens = tokens.pop(0)
+    def parse_factor(self, tokens):
+        """
+        Parse the factors as defined in the example
+        comments at the start of the file:
+
+        factor -> ( expression ) | number
+        """    
+        token = tokens.pop(0)
+        if token == "(":
+            result = self.parse_expression(tokens)
+            tokens = tokens.pop(0)
+            return result
+        else:
+            return float(token)
+
+    def parse_term(self, tokens):
+        """
+        Parse the terms as defined in the example
+        comments at the start of the file:
+
+        term -> factor * term | factor / term | factor
+        """
+        left = self.parse_factor(tokens)
+        while tokens and tokens[0] in ["*", "/"]:
+            op = tokens.pop(0)
+            right = self.parse_factor(tokens)
+            if op == "*":
+                left *= right
+            else:
+                left /= right
+        return left
+
+    def parse_expression(self, tokens):
+        """
+        Parse the expression as defined in the example
+        comments at the start of the file:
+
+        expression -> term + expression | term - expression | term
+        """
+        left = self.parse_term(tokens)
+        while tokens and tokens[0] in ["+", "-"]:
+            op = tokens.pop(0)
+            right = self.parse_term(tokens)
+            if op == "+":
+                left += right
+            else:
+                left -= right
+        return left
+
+    def evaluate_expression(self, expression):
+        """
+        Evaluate the expression by tokenizing it
+        and then parsing it.
+        """
+        tokens = self.tokenize(expression)
+        result = self.parse_expression(tokens)
         return result
-    else:
-        return float(token)
 
-def parse_term(tokens):
-    """
-    Parse the terms as defined in the example
-    comments at the start of the file:
-
-    term -> factor * term | factor / term | factor
-    """
-    left = parse_factor(tokens)
-    while tokens and tokens[0] in ["*", "/"]:
-        op = tokens.pop(0)
-        right = parse_factor(tokens)
-        if op == "*":
-            left *= right
-        else:
-            left /= right
-    return left
-
-def parse_expression(tokens):
-    """
-    Parse the expression as defined in the example
-    comments at the start of the file:
-
-    expression -> term + expression | term - expression | term
-    """
-    left = parse_term(tokens)
-    while tokens and tokens[0] in ["+", "-"]:
-        op = tokens.pop(0)
-        right = parse_term(tokens)
-        if op == "+":
-            left += right
-        else:
-            left -= right
-    return left
-
-def evaluate_expression(expression):
-    """
-    Evaluate the expression by tokenizing it
-    and then parsing it.
-    """
-    tokens = tokenize(expression)
-    result = parse_expression(tokens)
-    return result
-
-def main():
-    """
-    Main function to run the program.
-    """
-    expression = input("Enter an expression: ")
-    result = evaluate_expression(expression)
-    print(result)
+    def main(self):
+        """
+        Main function to run the program.
+        """
+        expression = input("Enter an expression: ")
+        result = self.evaluate_expression(expression)
+        print(result)
 
 if __name__ == "__main__":
-    main()
+    new_parser = RecursiveDescentParser()
+    new_parser.main()
         
 
