132fd9f08f860c5b859d2333db25c0ed5bb51eac
Andy Dennis <8628078+rpigu-i@users.noreply.github.com>
2026-01-02 16:14:19 -0500

    Quick updates to ch4 scripts

--- blackhat-python/ch4/pic_carver.py
+++ blackhat-python/ch4/pic_carver.py
@@ -8,6 +8,67 @@ pictures_directory =  "./pics"
 faces_directory = "./pics/faces"
 pcap_file = "bhp.pcap"
 
+
+def get_http_headers(http_payload):
+
+    try:
+        # split the headers off if it is HTTP traffic
+        headers_raw = http_payload[:http_payload.index("\r\n\r")+2]
+
+        # break out the headers
+        headers = dict(re.findall(r"(?P<name>.*?): (?P<value>.*?)\r\n", headers_raw))
+    except:
+        return None
+
+    return headers
+
+def extract_image(headers, http_payload):
+
+    image      = None
+    image_type = None
+
+    try:
+        if "image" in headers['Content-Type']:
+           # grab the image type and image body 
+           image_type = headers['Content-Type'].split("/")[1]
+
+           image = http_payload[http_payload.index("\r\n\r")+4:]
+
+           # if we detect compression decompress image
+           try:
+               if "Content-Encoding" in headers.keys():
+                  if headers['Content-Encoding'] == "gzip":
+                     image = zlib.decompress(image, 16+zlib.MAX_WBITS)
+                  elif headers['Content-Encoding'] == "deflate":
+                     image = zlib.decompress(image)
+           except:
+               pass
+    except:
+        return None,None
+ 
+    return image,image_type
+
+def face_detect(path, file_name):
+
+    img     = cv2.imread(path)
+    cascade = cv2.CascadeClassifier("haarcascade_frontalface_alt.xml")
+    rects   = cascade.detectMultiScale(img, 1.3, 4, cv2.cv.CV_HAAR_SCALE_IMAGE,(20,20))
+    
+    if len(rects) == 0:
+        return False
+
+    rects[:, 2:] += rects[:, :2]
+    
+    # highlight the faces in the image
+    for x1,y1,x2,y2 in rects:
+        cv2.rectangle(img,(x1,y1),(x2,y2),(127,255,0),2)
+    
+    cv2.imwrite("%s/%s-%s" % (faces_directory,pcap_file,file_name),img)
+
+    return True  
+        
+
+
 def http_assembler(pcap_files):
 
     carved_images = 0
